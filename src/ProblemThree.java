7318f999fde837a3954ce4b906d1ec62ed4cd187
syndavis <tdavis70@student.highlands.edu>
2025-01-23 09:13:58 -0500

    Add files via upload

---
+++ src/ProblemThree.java
@@ -0,0 +1,5 @@
+public class ProblemThree {
+    public static void main(String[] args){
+
+    }
+}
--- src/ProblemTwo.java
+++ src/ProblemTwo.java
@@ -1,5 +1,10 @@
 public class ProblemTwo {
     public static void main(String[] args){
-        // Write your code here!
+        double base = 3.5;
+        double height = 2.1;
+        double area = base * height / 2;
+        System.out.print("The Area of a triangle with a base of 3.5 and height of 2.1 is: ");
+        System.out.print(area);
+
     }
 }
