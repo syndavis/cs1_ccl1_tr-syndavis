e3e5957cd3c9c47ff4c4187b88215ff0cf5507e5
syndavis <tdavis70@student.highlands.edu>
2025-01-24 19:53:37 -0500

    To resolve the formatting issue, first I took "1. Flour" and created its own System.out.print line. From there, I made every line as System.out.println instead of just System.out.print. System.out.println makes it so that each line of code is printed out on its own line, as opposed to System.out.print which continues the output message on the same line. Using \n within the Message would achieve the same result as println, however, I decided to omit it so that the code can look a bit cleaner. With that being said, I removed line 2 from saying "2. Eggs\n\n" as it created an additional space within the list, making the code look a bit unorganized.

--- src/ProblemOne.java
+++ src/ProblemOne.java
@@ -1,10 +1,10 @@
 public class ProblemOne {
     public static void main(String[] args){
-        System.out.println("Shopping List for a Cake:1. Flour");
-        System.out.print("2. Eggs\n\n");
-        System.out.print("3. Sugar");
-        System.out.print("4. Butter");
-        System.out.print("5. Frosting");
-        // test
+        System.out.println("Shopping List for a Cake:");
+        System.out.println("1. Flour");
+        System.out.println("2. Eggs");
+        System.out.println("3. Sugar");
+        System.out.println("4. Butter");
+        System.out.println("5. Frosting");
     }
 }
